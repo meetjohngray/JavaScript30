fedb31cfff356695199e437ed194f51c1ba05219
John Gray <john.edwin.gray@gmail.com>
2021-12-03 06:55:57 +1300

    add/remove class inelegantly

--- 01 - JavaScript Drum Kit/drum-kit/src/Key.js
+++ 01 - JavaScript Drum Kit/drum-kit/src/Key.js
@@ -1,26 +1,34 @@
-import { useRef } from 'react';
+import { useState, useRef } from 'react';
 import KeyboardEventHandler from 'react-keyboard-event-handler'
 
 function Key(props) {
+  const [play, setPlay] = useState(false)
+  // const toggleFunc = () => {
+  //   setPlay(!play)
+  //   }
+
+
   const key = props.data.key
   const letter = props.data.letter
   const sound = props.data.sound
   const audio = props.data.audio
   const audioRef = useRef(null)
-  // const src = `/sounds/${sound}.wav`
   
   return (
     <>
     <KeyboardEventHandler
     handleKeys={[letter]}
-    onKeyEvent={() => {
+    onKeyEvent={(e) => {
       audioRef.current.currentTime = 0;
       audioRef.current.play();
-      console.log(`do something upon keydown event of ${key}`)
+      setPlay(true)
+      setTimeout(()=> setPlay(false), 100)
       } 
     }
     />  
-    <div data-key={key} className="key" >
+    <div data-key={key} 
+      className= {"key " + (play ? 'playing' : '')} 
+    >
       <kbd>{letter}</kbd>
       <span className="sound">{sound}</span>
       <audio 
